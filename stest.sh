e27dbdcb3f39088f066302260accc1a089f2a548
Bill Chan <biaocy91@gmail.com>
2018-12-07 17:28:25 +0800

    Update stress test script

--- stest.sh
+++ stest.sh
@@ -23,6 +23,36 @@ BAREMETAL_MACS=('58:F9:87:7A:A9:73' '58:F9:87:7A:A9:74' '58:F9:87:7A:A9:75' '58:
 BAREMETAL_IMAGE="http://112.93.129.99/images/ubuntu-xenial-16.04.qcow2"
 BAREMETAL_IMAGE_CHECKSUM="f3e563d5d77ed924a1130e01b87bf3ec"
 
+function usage {
+  cat <<EOF
+    USAGE
+        `basename $0` [-d] [-i number_of_iteration] [-a ibmc_address] 
+                 [-u ibmc_user] [-p ibmc_password]
+
+    DESCRIPTION
+        Stress test script for ibmc.
+
+        -i number_of_iteration
+            Number of iteration, if not specified, will prompt for input
+        
+        -a ibmc_address
+            IBMC address, e.g. http://ip[:port], if not specified, will prompt for input
+        
+        -u ibmc_user
+            IBMC username, if not specified, will prompt for input
+
+        -p ibmc_password
+            IBMC password, if not specified, will prompt for input
+
+        -d
+            Delete node with name [$BAREMETAL_NAME] without prompt
+        
+        -h
+            Show this help message
+EOF
+    exit 1
+}
+
 function build_node {
     local NODE=$(openstack baremetal node create --name "$BAREMETAL_NAME" \
     --driver "ibmc" --driver-info ibmc_address="$BAREMETAL_IBMC_ADDR" \
@@ -89,37 +119,91 @@ function check_node_exist {
         # Not exist, do nothing
         return
     else
-        local MSG="A node with name [$BAREMETAL_NAME] exist, delete it or exit the program: "
-        echo $MSG
-        local CHOICE=('Delete it' 'Exit the program')
-        select YN in "${CHOICE[@]}"
-        do
-            case $YN in
-                ${CHOICE[0]})
-                    delete_node $BAREMETAL_NAME; break;
-                    ;;
-                ${CHOICE[1]})
-                    exit
-                    ;;
-                *)
-                    echo "Please select option in ($(eval echo {1..${#CHOICE[@]}}))"
-                    ;;
-            esac
-        done
+        if [[ "$DELETE_EXIST_NODE" -eq "1" ]]
+        then
+            delete_node $BAREMETAL_NAME
+            return
+        else
+            local MSG="A node with name [$BAREMETAL_NAME] exist, delete it or exit the program: "
+            echo $MSG
+            local CHOICE=('Delete it' 'Exit the program')
+            select YN in "${CHOICE[@]}"
+            do
+                case $YN in
+                    ${CHOICE[0]})
+                        delete_node $BAREMETAL_NAME; break;
+                        ;;
+                    ${CHOICE[1]})
+                        exit
+                        ;;
+                    *)
+                        echo "Please select OP in ($(eval echo {1..${#CHOICE[@]}}))"
+                        ;;
+                esac
+            done
+        fi
     fi
 }
 
-# Default number of iteration
-DEFAULT_NUM_ITER=100
 
-read -p "Enter number of iteration(default $DEFAULT_NUM_ITER): " NUM_ITER
+# Parse argments
+NO_ARGS=0 
+E_OPTERROR=85
+# Default number of iteration
+DEFAULT_NUM_ITER=2
+# Whether to delete exist node with name $BAREMETAL_NAME
+# 0: prompt if there exist node with name $BAREMETAL_NAME
+# 1: delete without prompt
+DELETE_EXIST_NODE=0
+
+if [ $# -eq "$NO_ARGS" ]
+then
+
+    read -p "Enter number of iteration(default $DEFAULT_NUM_ITER): " NUM_ITER
+    read -p "Enter ibmc address(default $BAREMETAL_IBMC_ADDR): " IBMC_ADDR
+    read -p "Enter ibmc user(default $BAREMETAL_IBMC_USER): " IBMC_USER
+    read -p "Enter ibmc password(default $BAREMETAL_IBMC_PASS): " IBMC_PASS
+else
+while getopts ":i:a:u:p:dh" OP
+do
+    case $OP in
+        i) 
+            NUM_ITER=$OPTARG
+            ;;
+        a)
+            BAREMETAL_IBMC_ADDR="$OPTARG"
+            ;;
+        u)
+            BAREMETAL_IBMC_USER="$OPTARG"
+            ;;
+        p)
+            BAREMETAL_IBMC_PASS="$OPTARG"
+            ;;
+        d)
+            DELETE_EXIST_NODE=1
+            ;;
+        *) 
+            usage
+            ;;
+    esac
+done
+fi  
+
+shift $(($OPTIND - 1))
+
 # Number of iteration
 NUM_ITER=${NUM_ITER:-$DEFAULT_NUM_ITER}
+BAREMETAL_IBMC_ADDR=${IBMC_ADDR:-$BAREMETAL_IBMC_ADDR}
+BAREMETAL_IBMC_USER=${IBMC_USER:-$BAREMETAL_IBMC_USER}
+BAREMETAL_IBMC_PASS=${IBMC_PASS:-$BAREMETAL_IBMC_PASS}
 
 # Check node with name $BAREMETAL_NAME exist
 check_node_exist
 
 echo "Number of iteration: $NUM_ITER"
+echo "IBMC address: $BAREMETAL_IBMC_ADDR"
+echo "IBMC username: $BAREMETAL_IBMC_USER"
+echo "IBMC password: $BAREMETAL_IBMC_PASS"
 echo "Report file: $FILE_REPORT"
 echo "Please wait until this program complete..."
 
